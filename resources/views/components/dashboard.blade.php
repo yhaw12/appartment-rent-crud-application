f029bdcdeb705d567e08b74b1f021cd8bc81455c
yhaw12 <yooxing11@gmail.com>
2024-02-07 23:03:00 +0000

    updtae

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -1,30 +1,31 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
-<body class="w-full h-screen px-20">
-  <nav>
-    <!-- Navbar content goes here -->
-    @include('partials.NavBar')
-  </nav>
-
-  <div class="main-content">
-    <aside>
-      <!-- Sidebar content goes here -->
+<body>
+  <div class="flex">
+    <!-- Sidebar -->
+    <div class="w-1/3">
       @include('partials.sidebar')
-    </aside>
-
-    <section>
-      <!-- Main content goes here -->
-      @yield('content')
-    </section>
+    </div>
+  
+    <div class="w-2/3">
+        <!-- Navbar -->
+        <div class="navbar">
+            Dashboard
+            @include('partials.NavBar')
+        </div>
+  
+        <!-- Content Area -->
+        <div class="content">
+          @yield('content')
+        </div>
+        
+      
+    </div>
   </div>
-
-  <footer class="bg-slate-500">
-    <!-- Footer content goes here -->
-  </footer>
 </body>
 </html>
